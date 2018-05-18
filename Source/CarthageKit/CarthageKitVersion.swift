bcee0e06118d0e120299d0d867030fee88aca4fc
Bart Whiteley <bwhiteley@gmail.com>
2018-05-18 09:55:50 -0600

    custom version for forked project

--- Source/CarthageKit/CarthageKitVersion.swift
+++ Source/CarthageKit/CarthageKitVersion.swift
@@ -3,4 +3,4 @@ public struct CarthageKitVersion {
 	public let value: SemanticVersion
 
 	public static let current = CarthageKitVersion(value: SemanticVersion(major: 0, minor: 29, patch: 0))
-}
+    }
