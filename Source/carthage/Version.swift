e982c12127bf8cd11557211420c56ac145c7b17a
Bart Whiteley <bwhiteley@gmail.com>
2018-05-19 17:56:23 -0600

    latest from fast-resolver PR

--- Source/carthage/Version.swift
+++ Source/carthage/Version.swift
@@ -9,7 +9,7 @@ public struct VersionCommand: CommandProtocol {
 	public let function = "Display the current version of Carthage"
 
 	public func run(_ options: NoOptions<CarthageError>) -> Result<(), CarthageError> {
-		carthage.println("\(CarthageKitVersion.current.value)_acom1")
+		carthage.println("\(CarthageKitVersion.current.value)_acom2")
 		return .success(())
 	}
 }
